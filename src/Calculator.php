cff69569de4c9d07917ecf31492a2e23aaf680f4
shimomo <developer@shimomo.net>
2023-11-08 06:38:10 +0900

    Initial commit

---
+++ src/Calculator.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Boatrace\Ninja;
+
+use DI\Container;
+use DI\ContainerBuilder;
+
+/**
+ * @author shimomo
+ */
+class Calculator
+{
+    /**
+     * @var \Boatrace\Ninja\MainCalculator
+     */
+    protected $calculator;
+
+    /**
+     * @var \Boatrace\Ninja\Calculator
+     */
+    protected static $instance;
+
+    /**
+     * @var \DI\Container
+     */
+    protected static $container;
+
+    /**
+     * @param  \Boatrace\Ninja\MainCalculator  $calculator
+     * @return void
+     */
+    public function __construct(MainCalculator $calculator)
+    {
+        $this->calculator = $calculator;
+    }
+
+    /**
+     * @param  string  $name
+     * @param  array   $arguments
+     * @return float
+     */
+    public function __call(string $name, array $arguments): float
+    {
+        return call_user_func_array([$this->calculator, $name], $arguments);
+    }
+
+    /**
+     * @param  string  $name
+     * @param  array   $arguments
+     * @return float
+     */
+    public static function __callStatic(string $name, array $arguments): float
+    {
+        return call_user_func_array([self::getInstance(), $name], $arguments);
+    }
+
+    /**
+     * @return \Boatrace\Ninja\Calculator
+     */
+    public static function getInstance(): Calculator
+    {
+        return self::$instance ?? self::$instance = (
+            self::$container ?? self::$container = self::getContainer()
+        )->get('Calculator');
+    }
+
+    /**
+     * @return \DI\Container
+     */
+    public static function getContainer(): Container
+    {
+        $builder = new ContainerBuilder;
+        $builder->addDefinitions(__DIR__ . '/../config/definitions.php');
+        return $builder->build();
+    }
+}
